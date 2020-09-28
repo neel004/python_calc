43d83d2b44ed80737620825b09e5c03cd420f17c
neel004 <neelp0004@gmail.com>
2020-09-28 15:04:02 +0530

    Created : main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+
+from PyQt5.QtWidgets import QApplication
+from view import GUI
+from  model import evaluateExpression
+
+# Client code
+def main():
+    """Main function."""
+    # Create an instance of QApplication
+    pycalc = QApplication(sys.argv)
+    # Show the calculator's GUI
+    view = GUI()
+    view.show()
+
+    model = evaluateExpression
+    sys.exit(pycalc.exec_())
+
+if __name__ == '__main__':
+    main()
